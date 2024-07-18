be98ce121d8aa7fc697b45747936b036c109d3e5
peter88213 <petert88213@gmail.com>
2024-07-18 08:26:36 +0200

    Fix docstring

--- src/nvmatrixlib/matrix_button.py
+++ src/nvmatrixlib/matrix_button.py
@@ -1,7 +1,7 @@
 """Provide a class for a Matrix button.
 
 Copyright (c) 2024 Peter Triesberger
-For further information see https://github.com/peter88213/nv_clipboard
+For further information see https://github.com/peter88213/nv_matrix
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
 from tkinter import ttk
