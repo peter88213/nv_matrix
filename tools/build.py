1b5b71e89ada4ebb259e854d3a35f2c620781dd3
peter88213 <petert88213@gmail.com>
2024-09-09 00:53:05 +0200

    Provide a Python build script.

--- tools/build.py
+++ tools/build.py
@@ -1,9 +1,9 @@
-"""Build a matrix novelibre plugin.
+"""Build the nv_matrix novelibre plugin package.
         
 In order to distribute a single script without dependencies, 
 this script "inlines" all modules imported from the novxlib package.
 
-The novxlib project (see https://github.com/peter88213/novxlib)
+The novxlib project (see see https://github.com/peter88213/novxlib)
 must be located on the same directory level as the nv_matrix project. 
 
 Copyright (c) 2024 Peter Triesberger
@@ -11,26 +11,32 @@ For further information see https://github.com/peter88213/nv_matrix
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
 import os
+from shutil import copy2
 import sys
+
 sys.path.insert(0, f'{os.getcwd()}/../../novxlib/src')
-import inliner
+from package_builder import PackageBuilder
 
-SOURCE_DIR = '../src/'
-TEST_DIR = '../test/'
-SOURCE_FILE = f'{SOURCE_DIR}nv_matrix.py'
-TEST_FILE = f'{TEST_DIR}nv_matrix.py'
+VERSION = '4.4.1'
 
 
-def inline_modules():
-    inliner.run(SOURCE_FILE, TEST_FILE, 'nvmatrixlib', '../../nv_matrix/src/')
-    inliner.run(TEST_FILE, TEST_FILE, NVLIB, NV_PATH)
-    inliner.run(TEST_FILE, TEST_FILE, 'novxlib', '../../novxlib/src/', copynovxlib=False)
-    print('Done.')
+class PluginBuilder(PackageBuilder):
+
+    PRJ_NAME = 'nv_matrix'
+    LOCAL_LIB = 'nvmatrixlib'
+    GERMAN_TRANSLATION = True
+
+    def add_extras(self):
+        self.add_icons()
+
+    def add_icons(self):
+        super().add_icons()
+        copy2('../icons/mLogo32.png', f'{self.buildDir}/icons')
 
 
 def main():
-    os.makedirs(TEST_DIR, exist_ok=True)
-    inline_modules()
+    pb = PluginBuilder(VERSION)
+    pb.run()
 
 
 if __name__ == '__main__':
